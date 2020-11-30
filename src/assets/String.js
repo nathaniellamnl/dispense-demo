b09847784ff4f8d0355dbbc806d7344238adef04
nathaniellamnl <nl.nathanlam@gmail.com>
2020-11-30 23:15:11 +0800

    About to deply

--- src/assets/String.js
+++ src/assets/String.js
@@ -1,3 +1,2 @@
-// exports.graphqlServerUrl='http://localhost:8000/graphql';
-exports.graphqlServerUrl='https://whispering-basin-06963.herokuapp.com//graphql';
+exports.graphqlServerUrl='http://localhost:8000/graphql';
 
