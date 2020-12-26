feba8dfe4d1cf08e6d1fca340e69116d2561bee8
nathaniellamnl <nl.nathanlam@gmail.com>
2020-12-26 22:12:16 +0800

    Fixed configuration

--- src/components/DrugInfo/DrugInfo.js
+++ src/components/DrugInfo/DrugInfo.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState, useEffect, useCallback, Suspense } from 'react';
+import React, { Fragment, useState, useEffect, Suspense } from 'react';
 import { Paper, TableBody, TableCell, TableRow, IconButton } from '@material-ui/core';
 import DeleteIcon from '@material-ui/icons/Delete'
 import EditIcon from '@material-ui/icons/Edit';
