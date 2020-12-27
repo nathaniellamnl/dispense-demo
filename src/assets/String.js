30610b62b616eb3fbd004fe09749de8fd00176df
nathaniellamnl <nl.nathanlam@gmail.com>
2020-12-27 15:29:15 +0800

    Changed the server url

--- src/assets/String.js
+++ src/assets/String.js
@@ -1,3 +1,1 @@
-// exports.graphqlServerUrl='https://localhost:8000/graphql';
-
-exports.graphqlServerUrl= "https://arcane-earth-96378.herokuapp.com/graphql";
+exports.graphqlServerUrl='https://localhost:8000/graphql';
